3c6d38e355bad186b010ed2649ad4be41d251269
Russell Sherman <russell.t.sherman@gmail.com>
2016-04-26 22:41:44 -0500

    08-react-bootstrap
    # Conflicts:
    #	app/src/components/App.jsx
    #	package.json
    #	webpack.config.js

--- app/src/components/App.jsx
+++ app/src/components/App.jsx
@@ -1,43 +1,34 @@
-import React, { Component } from 'react';
-import { Router, Route, Link, hashHistory } from 'react-router';
+import React, {Component} from 'react';
+import {Router, Route, Link, hashHistory} from 'react-router';
 import Heading from './Heading.jsx';
 import StateUpdate from './StateUpdate.jsx';
+import Bootstrap from './Bootstrap.jsx';
 
-const Home = () => <div><Links /><Heading txt="Home" /></div>;
-const About = () => <div><Links /><Heading txt="About" /></div>;
-const Contact = (props) =>
-  <div>
-    <Links />
-    <Heading txt="Contact" />
-    <UserLinks />
-    {props.children}
-  </div>;
-const Message = (props) =>
-  <div>
-    <Heading txt={props.params.user} />
-    <StateUpdate />
-  </div>;
+const Home = () => <div><Links /><Heading txt='Home' /><Bootstrap /></div>;
+const About = () => <div><Links /><Heading txt='About' /></div>;
+const Contact = (props) => <div><Links /><Heading txt='Contact' /><UserLinks />{props.children}</div>;
+const Message = (props) => <div><Heading txt={props.params.user} /><StateUpdate /></div>;
 const Links = () =>
   <nav>
-    <Link to="/">Home</Link><br />
-    <Link to="/about">About</Link><br />
-    <Link to="/contact">Contact</Link><br />
+    <Link to='/'>Home</Link><br />
+    <Link to='/about'>About</Link><br />
+    <Link to='/contact'>Contact</Link><br />
   </nav>;
 const UserLinks = () =>
   <nav>
-    <Link to="/contact/bob">Contact Bob</Link><br />
-    <Link to="/contact/john">Contact John</Link><br />
-    <Link to="/contact/mary">Contact Mary</Link>
+    <Link to='/contact/bob'>Contact Bob</Link><br />
+    <Link to='/contact/john'>Contact John</Link><br />
+    <Link to='/contact/mary'>Contact Mary</Link>
   </nav>;
 
 
 const App = function app() {
   return (
     <Router history={hashHistory}>
-      <Route path="/" component={Home} />
-      <Route path="/about" component={About} />
-      <Route path="/contact" component={Contact}>
-        <Route path="(:user)" component={Message} />
+      <Route path='/' component={Home} />
+      <Route path='/about' component={About} />
+      <Route path='/contact' component={Contact}>
+        <Route path='(:user)' component={Message} />
       </Route>
     </Router>
   );
